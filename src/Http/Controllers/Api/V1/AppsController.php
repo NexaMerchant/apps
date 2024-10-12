ebcd15068f64ea83a30aeba7b57259a1fe304ef9
Steve <34465153+xxl4@users.noreply.github.com>
2024-10-12 14:42:15 +0800

    add more api for apps

--- src/Http/Controllers/Api/V1/AppsController.php
+++ src/Http/Controllers/Api/V1/AppsController.php
@@ -4,9 +4,21 @@ namespace NexaMerchant\Apps\Http\Controllers\Api\V1;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 
 use NexaMerchant\Apps\Http\Controllers\Api\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class AppsController extends Controller
 {
+
+    use ValidatesRequests;
+
+    private $storage;
+
+    private $cache_expire = 60;
+
+    public function __construct() {
+        parent::__construct();
+    }
     
     /**
      * 
@@ -55,6 +67,112 @@ class AppsController extends Controller
         $data = [];
         $data['code'] = 200;
         $data['message'] = "success";
+        return response()->json($data);
+    }
+
+    /**
+     * 
+     * Get The Apps List
+     * @return json
+     * @param Request $request
+     * @access public
+     * 
+     * @auther NexaMerchant
+     * 
+     */
+    public function index(Request $request) {
+
+        $name = $request->input('name');
+        $limit = $request->input('limit', 10);
+        $page = $request->input('page', 1);
+        $type = $request->input('type', 'all');
+        $sort = $request->input('sort', 'desc');
+        $order = $request->input('order', 'created_at');
+        $category = $request->input('category');
+
+        $query = \NexaMerchant\Apps\Models\App::query();
+        if($name) {
+            $query->where('name', 'like', "%$name%");
+        }
+
+        if($type != 'all') {
+            $query->where('type', $type);
+        }
+
+        if($category) {
+            $query->where('category', $category);
+        }
+
+        $query->orderBy($order, $sort);
+
+        $data = $query->paginate($limit, ['*'], 'page', $page);
+
+        $data = [];
+        $data['code'] = 200;
+        $data['message'] = "success";
+        return response()->json($data);
+    }
+
+    /**
+     * 
+     * Get The Apps Detail
+     * @return json
+     * @param Request $request
+     * @access public
+     * 
+     * @auther NexaMerchant
+     * 
+     */
+    public function show(Request $request, $id) {
+
+        // add cache for the app detail
+        $app = Cache::remember('apps_detail_' . $id, $this->cache_expire, function() use ($id) {
+            return \NexaMerchant\Apps\Models\App::where('id', $id)->first();
+        });
+
+        $data = [];
+        $data['code'] = 200;
+        $data['message'] = "success";
+        $data['data'] = $app;
+        return response()->json($data);
+    }
+
+    /**
+     * 
+     * Get The Apps Platform Category
+     * @return json
+     */
+    public function category() {
+
+        // add cache for the category
+
+        $category = Cache::remember('apps_category', $this->cache_expire, function() {
+            return \NexaMerchant\Apps\Models\App::select('category')->groupBy('category')->get();
+        });
+
+        $data = [];
+        $data['code'] = 200;
+        $data['message'] = "success";
+        $data['data'] = $category;
+        return response()->json($data);
+    }
+
+    /**
+     * 
+     * Get The Apps Platform Type
+     * @return json
+     */
+    public function type() {
+
+        // add cache for the type
+        $type = Cache::remember('apps_type', $this->cache_expire, function() {
+            return \NexaMerchant\Apps\Models\App::select('type')->groupBy('type')->get();
+        });
+
+        $data = [];
+        $data['code'] = 200;
+        $data['message'] = "success";
+        $data['data'] = $type;
         return response()->json($data);
     }
 
--- src/Routes/api.php
+++ src/Routes/api.php
@@ -28,8 +28,50 @@ Route::group(['middleware' => ['api'], 'prefix' => 'api'], function () {
             Route::controller(AppsController::class)->prefix('apps')->group(function () {
     
                 Route::get('demo', 'demo')->name('apps.api.v1.apps.demo');
+
+                Route::post('publish', 'publish')->name('apps.api.v1.apps.publish');
+
+                Route::post('upload', 'upload')->name('apps.api.v1.apps.upload');
+
+                Route::get('download', 'download')->name('apps.api.v1.apps.download');
+
+                Route::get('list', 'list')->name('apps.api.v1.apps.list');
+
+                Route::get('info', 'info')->name('apps.api.v1.apps.info');
+
+                Route::get('delete', 'delete')->name('apps.api.v1.apps.delete');
+
+                Route::get('install', 'install')->name('apps.api.v1.apps.install');
+
+                Route::get('uninstall', 'uninstall')->name('apps.api.v1.apps.uninstall');
+
+                Route::get('enable', 'enable')->name('apps.api.v1.apps.enable');
+
+                Route::get('disable', 'disable')->name('apps.api.v1.apps.disable');
+
+                Route::get('update', 'update')->name('apps.api.v1.apps.update');
+
+                Route::get('rollback', 'rollback')->name('apps.api.v1.apps.rollback');
+
+                Route::get('status', 'status')->name('apps.api.v1.apps.status');
+
+                Route::get('logs', 'logs')->name('apps.api.v1.apps.logs');
+
+                Route::get('clear', 'clear')->name('apps.api.v1.apps.clear');
+
+                Route::get('config', 'config')->name('apps.api.v1.apps.config');
+
+                Route::get('setting', 'setting')->name('apps.api.v1.apps.setting');
+
+                Route::get('show', 'show')->name('apps.api.v1.apps.show');
+
+                Route::get('type', 'type')->name('apps.api.v1.apps.type');
+
+                Route::get('category', 'category')->name('apps.api.v1.apps.category');
     
             });
+
+
     
         });
 
